1cae2801acfcda3e1a05a0cdc37f708dd356131d
hamdy-fathi <hamdyfathi2@gmail.com>
2026-02-01 17:40:42 +0200

    change env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   API_VERSION: 'v1',
-  API_URL: `https://api.vdentaleg.com/api`,
-  Domain_URL: `https://api.vdentaleg.com/`,
+  API_URL: `http://92.113.31.86/api`,
+  Domain_URL: `http://92.113.31.86/`,
 };
